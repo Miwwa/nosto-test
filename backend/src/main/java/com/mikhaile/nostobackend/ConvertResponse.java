cb6f691b13c0b6c878a1600ed1f65b40b1d880f0
Mikhail <eremin.ms92@gmaill.com>
2025-02-03 22:28:19 +0200

    Refactor ConvertResponse and add request validation.
    Replaced ConvertResponse class with a concise record structure. Introduced validation for API requests using Vert.x Web Validation, ensuring proper handling of query and path parameters. Enhanced error handling to provide detailed responses for validation failures and unexpected errors.

--- backend/src/main/java/com/mikhaile/nostobackend/ConvertResponse.java
+++ backend/src/main/java/com/mikhaile/nostobackend/ConvertResponse.java
@@ -1,15 +1,9 @@
 package com.mikhaile.nostobackend;
 
-class ConvertResponse {
-    public String baseCurrency;
-    public String quoteCurrency;
-    public Float baseAmount;
-    public Float quoteAmount;
-
-    public ConvertResponse(String baseCurrency, String quoteCurrency, Float baseAmount, Float quoteAmount) {
-        this.baseCurrency = baseCurrency;
-        this.quoteCurrency = quoteCurrency;
-        this.baseAmount = baseAmount;
-        this.quoteAmount = quoteAmount;
-    }
+record ConvertResponse(
+    String baseCurrency,
+    String quoteCurrency,
+    Float baseAmount,
+    Float quoteAmount
+) {
 }
--- backend/src/main/java/com/mikhaile/nostobackend/MainVerticle.java
+++ backend/src/main/java/com/mikhaile/nostobackend/MainVerticle.java
@@ -3,27 +3,66 @@ package com.mikhaile.nostobackend;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.core.http.HttpServer;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.validation.ValidationHandler;
+import io.vertx.ext.web.validation.builder.Parameters;
+import io.vertx.ext.web.validation.builder.ValidationHandlerBuilder;
+import io.vertx.json.schema.*;
 
-import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static io.vertx.json.schema.common.dsl.Schemas.numberSchema;
+import static io.vertx.json.schema.common.dsl.Schemas.stringSchema;
+import static io.vertx.json.schema.draft7.dsl.Keywords.minimum;
 
 public class MainVerticle extends AbstractVerticle {
+
+    ConcurrentHashMap<String, ConcurrentHashMap<String, Float>> cache = new ConcurrentHashMap<>();
 
     @Override
     public void start(Promise<Void> startPromise) throws Exception {
         HttpServer server = vertx.createHttpServer();
         Router router = Router.router(vertx);
 
+        WebClient httpClient = WebClient.create(vertx);
+
         router
             .get("/api/convert/:baseCurrency/:quoteCurrency")
-            .handler(ctx -> {
-                String baseCurrency = ctx.pathParam("baseCurrency");
-                String quoteCurrency = ctx.pathParam("quoteCurrency");
-                List<String> amountStr = ctx.queryParam("amount");
-                ConvertResponse res = new ConvertResponse(baseCurrency, quoteCurrency, 1.0f, 1.2345f);
-                ctx.json(res);
-            });
+            .handler(buildConvertValidationHandler())
+            .handler(this::convertRequestHandler)
+            .failureHandler(this::errorHandler);
 
         server.requestHandler(router).listen(8888);
+    }
+
+    private ValidationHandler buildConvertValidationHandler() {
+        SchemaRouter schemaRouter = SchemaRouter.create(vertx, new SchemaRouterOptions());
+        SchemaParser parser = SchemaParser.createDraft7SchemaParser(schemaRouter);
+        return ValidationHandlerBuilder.create(parser)
+            .pathParameter(Parameters.param("baseCurrency", stringSchema()))
+            .pathParameter(Parameters.param("quoteCurrency", stringSchema()))
+            .queryParameter(Parameters.param("amount", numberSchema().with(minimum(0))))
+            .build();
+    }
+
+    private void convertRequestHandler(RoutingContext ctx) {
+        String baseCurrency = ctx.pathParam("baseCurrency");
+        String quoteCurrency = ctx.pathParam("quoteCurrency");
+
+        // `amount` is guaranteed to be valid since validation handler ensures it
+        float amount = Float.parseFloat(ctx.queryParam("amount").get(0));
+
+        ConvertResponse res = new ConvertResponse(baseCurrency, quoteCurrency, amount, amount * 1.2345f);
+        ctx.json(res);
+    }
+
+    private void errorHandler(RoutingContext ctx) {
+        ctx.json(new JsonObject()
+            .put("error", ctx.statusCode() == 400 ? "Bad Request" : "Unexpected Error")
+            .put("message", ctx.failure().getMessage())
+        );
     }
 }
